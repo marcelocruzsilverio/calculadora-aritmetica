f646de61c5bfe6573b8eac2322bfd8fe9fbf441b
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2023-07-15 20:16:58 -0300

    finalizado o css

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,137 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container container-flex">
+    <div class="content">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <header>
+        <h1>Calculadora Aritmética</h1>
+      </header>
+      <form>
+        <h2>Escolha a operação desejada</h2>
+        <select>
+          <option value="somar">Somar</option>
+          <option value="subtrair">Subtrair</option>
+          <option value="multiplicar">Multiplicar</option>
+          <option value="dividir">Dividir</option>
+        </select>
+        <input type="number" placeholder="digite o primeiro número">
+        <span>+</span>
+        <input type="number" placeholder="digite o segundo número">
+        <label for="">Resultado: </label>
+        <input type="number">
+      </form>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Roboto, sans-serif;
+
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+body {}
+
+.container {
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+  height: 100vh;
+
+
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.container-flex {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.content {
+  background-color: #07245a;
+  padding: 24px;
+  border-radius: 16px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+header h1 {
+  margin-bottom: 24px;
+  text-align: center;
+  font-size: 2.5rem;
+  color: #fff;
+
+}
+
+select {
+  margin-bottom: 16px;
+  padding: 8px;
+  border-radius: 12px;
+  outline-color: #77a2f3;
+  border: none;
+  font-size: 1rem;
+  color: #072f79;
+  font-weight: bold;
+  text-align: center;
+  appearance: none;
+}
+
+select option {
+  font-size: 1rem;
+  color: #072f79;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
+}
+
+form input {
+  outline-color: #77a2f3;
+  margin: 16px 0;
+  padding: 16px;
+  border-radius: 12px;
+  border: none;
+  font-size: 1rem;
+  text-align: center;
+
+}
+
+
+form input:hover {
+  background-color: #77a2f3;
+}
+
+form input::placeholder {
+  font-size: 1rem;
+  color: #072f79;
+}
+
+form input[type="number"]::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+}
+
+form h2 {
+  text-align: center;
+  margin-bottom: 16px;
+  color: #c3c3c3;
+  font-size: 1.5rem;
+}
+
+form label {
+  color: #fff;
+  border-bottom: 1px solid #fff;
+}
+
+span {
+  font-size: 1.5rem;
+  color: #fff;
 }
 </style>
