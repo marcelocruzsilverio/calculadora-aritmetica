65deed0fe2e5ce64f2354b07bdfca5364a01c10b
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2023-07-16 01:05:38 -0300

    finalizado app em um arquivo

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,95 @@
 <script setup>
+import { reactive } from 'vue';
+
+
+const state = reactive({
+  filter: 'choose',
+  firstNumber: 0,
+  secondeNumber: 0,
+  message: 'Digite um valor'
+})
+
+
+const alternateOperationSymbol = () => {
+  const filter = state.filter;
+
+  switch (filter) {
+    case 'sum':
+      return '+';
+    case 'subtract':
+      return '-';
+    case 'multiply':
+      return '*'
+    case 'divide':
+      return '/'
+  }
+}
+
+const sum = () => Number(state.firstNumber) + Number(state.secondeNumber);
+const subtract = () => Number(state.firstNumber) - Number(state.secondeNumber);
+const multiply = () => Number(state.firstNumber) * Number(state.secondeNumber);
+const divide = () => {
+  if (state.firstNumber > 0) {
+    let result = 0
+    result = Number(state.firstNumber) / Number(state.secondeNumber);
+    return result.toFixed(2)
+  } else {
+    return '0'
+  }
+}
+
+const operations = () => {
+  const filter = state.filter;
+
+
+  switch (filter) {
+    case 'sum':
+      return sum();
+    case 'subtract':
+      return subtract();
+    case 'multiply':
+      return multiply();
+    case 'divide':
+      return divide();
+  }
+
+}
+
+const showResult = () => {
+  const filter = state.filter
+  if (filter === 'choose') {
+    return 'choose'
+  } else {
+    return '';
+  }
+}
+
 
 </script>
 
 <template>
   <div class="container container-flex">
     <div class="content">
-
       <header>
         <h1>Calculadora Aritmética</h1>
       </header>
       <form>
         <h2>Escolha a operação desejada</h2>
-        <select>
-          <option value="somar">Somar</option>
-          <option value="subtrair">Subtrair</option>
-          <option value="multiplicar">Multiplicar</option>
-          <option value="dividir">Dividir</option>
+        <select @change="event => state.filter = event.target.value">
+          <option value="choose" selected>Selecionar</option>
+          <option value="sum">Somar</option>
+          <option value="subtract">Subtrair</option>
+          <option value="multiply">Multiplicar</option>
+          <option value="divide">Dividir</option>
         </select>
-        <input type="number" placeholder="digite o primeiro número">
-        <span>+</span>
-        <input type="number" placeholder="digite o segundo número">
-        <label for="">Resultado: </label>
-        <input type="number">
+        <input @keyup="event => state.firstNumber = event.target.value" type="number"
+          placeholder="digite o primeiro valor">
+        <span v-text="alternateOperationSymbol()"></span>
+        <input @keyup="event => state.secondeNumber = event.target.value" type="number"
+          placeholder="digite o segundo valor">
+        <label>Resultado: </label>
+        <!-- <input type="number" v-model="operations"> -->
+        <span class="result" v-text="operations()" v-if="showResult() !== 'choose'"></span>
       </form>
     </div>
   </div>
@@ -36,7 +104,6 @@
 
 }
 
-body {}
 
 .container {
   max-width: 1024px;
@@ -58,6 +125,7 @@ body {}
   background-color: #07245a;
   padding: 24px;
   border-radius: 16px;
+  width: 70%;
 }
 
 header h1 {
@@ -101,6 +169,8 @@ form input {
   border: none;
   font-size: 1rem;
   text-align: center;
+  -moz-appearance: textfield;
+  appearance: textfield;
 
 }
 
@@ -114,7 +184,8 @@ form input::placeholder {
   color: #072f79;
 }
 
-form input[type="number"]::-webkit-inner-spin-button {
+form input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
   -webkit-appearance: none;
 }
 
@@ -128,10 +199,32 @@ form h2 {
 form label {
   color: #fff;
   border-bottom: 1px solid #fff;
+  margin-bottom: 8px;
 }
 
 span {
   font-size: 1.5rem;
   color: #fff;
+}
+
+.result {
+  border: 1px solid #77a2f3;
+  padding: 16px;
+  background-color: #12e752;
+  color: #07245a;
+}
+
+@media (max-width: 700px) {
+  .content {
+    width: 90%;
+  }
+
+  header h1 {
+    font-size: 2rem;
+  }
+
+  form h2 {
+    font-size: 1.2rem;
+  }
 }
 </style>
